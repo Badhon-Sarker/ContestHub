ec2c4787438b241e9b1aa20de20ebff680151110
Badhon-Sarker <badhonsarker38@gmail.com>
2024-06-05 00:35:13 +0600

    role button added

--- src/Components/Dashbord/Admin/MangeUser/ManageUser.jsx
+++ src/Components/Dashbord/Admin/MangeUser/ManageUser.jsx
@@ -1,9 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { FaTrash } from "react-icons/fa";
+import UserRoleModal from "../../../UserRoleModal/UserRoleModal";
+import { useState } from "react";
 
 const ManageUser = () => {
   const isBlocked = true;
+  const [isOpen, setIsOpen] = useState(false)
   const { data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
@@ -12,8 +15,20 @@ const ManageUser = () => {
     },
   });
 
+
+  
+  
+
+
+  const handleModal = (selected) =>{
+    console.log('handle role updated', selected)
+  }
+
   return (
     <div>
+      <div>
+        <h1 className="text-3xl font-bold my-5">Manage user</h1>
+      </div>
       <div>
         <div className="overflow-x-auto">
           <table className="table">
@@ -24,6 +39,7 @@ const ManageUser = () => {
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
+                <th>Update Role</th>
                 <th>Delete</th>
                 <th>Block</th>
               </tr>
@@ -31,13 +47,15 @@ const ManageUser = () => {
             <tbody>
               {/* row 1 */}
               {users.map((user, idx) => (
+                
                 <tr key={user._id} className=" hover:bg-gray-200">
                   <td>{idx + 1}</td>
                   <td>{user.name}</td>
                   <td>{user.email}</td>
-                  <td>
-                    User
-                  </td>
+                  <td>{user.role}</td>
+                  <td><button className="btn" onClick={()=> setIsOpen(true)}>Update Role</button>
+                  <UserRoleModal setIsOpen={setIsOpen} isOpen={isOpen} handleModal={handleModal} user={user}></UserRoleModal></td>
+                  
                   <td>
                     <button className="bg-red-200 text-red-500 btn">
                       <FaTrash />
@@ -45,7 +63,7 @@ const ManageUser = () => {
                   </td>
                   {isBlocked ? (
                     <td>
-                      <button className="btn bg-[#B0E0E6]">Unblock</button>
+                      <button  className="btn bg-[#B0E0E6]">Unblock</button>
                     </td>
                   ) : (
                     <td>
@@ -53,6 +71,7 @@ const ManageUser = () => {
                     </td>
                   )}
                 </tr>
+                
               ))}
             </tbody>
           </table>
